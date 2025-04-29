1cbec1a8f91971976f4005c10359c8977eec93f5
Vinícius Zanfelici <86609742+vinizaan@users.noreply.github.com>
2025-04-29 20:02:19 -0300

    parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Retrofit
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 }
---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
@@ -0,0 +1,28 @@
+package br.edu.ifsp.scl.sdm.currencyconverter.model.api
+
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.ConversionResult
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.CurrencyList
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface CurrencyConverterApiService {
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: dcfb8bc310msh55d95907c0ee9b3p1f2c26jsn4e7c3185fe03"
+    )
+    @GET("list")
+    fun getCurrencies(): Call<CurrencyList>
+
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: dcfb8bc310msh55d95907c0ee9b3p1f2c26jsn4e7c3185fe03"
+    )
+    @GET("convert")
+    fun convert(
+        @Query("from") from: String,
+        @Query("to") to: String,
+        @Query("amount") amount: String
+    ): Call<ConversionResult>
+}
